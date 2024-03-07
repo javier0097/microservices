77df59dcb1d0bf316ee346cd4e20c6867f1dd3a8
Javier Martinez <javier0097@hotmail.com>
2024-03-07 10:42:24 -0400

    eureka configuration

--- users-service/src/main/java/com/example/application/Application.java
+++ users-service/src/main/java/com/example/application/Application.java
@@ -3,6 +3,7 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
