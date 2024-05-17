7f8a8d74dfc3c8b5d5bf1bf1b0d5bf29f2fa685f
Javier Martinez <javier0097@hotmail.com>
2024-05-17 13:17:59 -0400

    config server created

---
+++ config service/src/test/java/com/example/application/service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.application.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
